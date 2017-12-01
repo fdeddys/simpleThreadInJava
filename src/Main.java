511efbcab898bdb85e8fd1a2feb67bf37ee0f8c9
deddy syuhendra <fransiscus.deddy@gmail.com>
2017-12-01 14:57:33 +0700

    update runnable

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
         Thread tr = new Thread("NEW Thread 2"){
             @Override
             public void run() {
@@ -25,13 +23,15 @@ public class Main {
         };
         tr.start();
 
-        jalankanThread();
+        Thread tr2 = new Thread(new MyRunnable());
+        tr2.start();
 
+        jalankanThread();
     }
 
     private static void jalankanThread(){
         nextNumb++;
-        SimpelThread simpelThread = new SimpelThread("tes Simple " + nextNumb);
+        SimpelThread simpelThread = new SimpelThread("SimpelThread " + nextNumb);
         simpelThread.start();
     }
 
@@ -41,7 +41,6 @@ public class Main {
             super(name);
             this.setName(name);
         }
-
         @Override
         public void run() {
             super.run();    //To change body of overridden methods use File | Settings | File Templates.
@@ -55,14 +54,28 @@ public class Main {
             } catch (InterruptedException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
-
         }
         @Override
         public synchronized void start() {
             super.start();    //To change body of overridden methods use File | Settings | File Templates.
             System.out.println("thread no " + nextNumb + " started ");
         }
+    }
 
+    private static class MyRunnable implements Runnable{
+
+        @Override
+        public void run() {
+
+            try {
+                for (int i=1; i<=10; i++){
+                    System.out.println(this.getClass().getName() + " " + i);
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
